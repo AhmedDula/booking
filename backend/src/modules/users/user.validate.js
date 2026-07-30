7712ae8fe1b4b1a9226cf6453eef39ff025b3077
Shrouk Gamal <sheroukgamal82@gmail.com>
2026-07-30 20:37:52 +0300

    add user validation and improve user module

---
+++ backend/src/modules/users/user.validate.js
@@ -0,0 +1,35 @@
+const {
+  validateCreate,
+  validateUpdate,
+} = require("./user.validation");
+
+const validateCreateUser = (req, res, next) => {
+  const errors = validateCreate(req.body);
+
+  if (errors.length) {
+    return res.status(400).json({
+      success: false,
+      errors,
+    });
+  }
+
+  next();
+};
+
+const validateUpdateUser = (req, res, next) => {
+  const errors = validateUpdate(req.body);
+
+  if (errors.length) {
+    return res.status(400).json({
+      success: false,
+      errors,
+    });
+  }
+
+  next();
+};
+
+module.exports = {
+  validateCreateUser,
+  validateUpdateUser,
+};
