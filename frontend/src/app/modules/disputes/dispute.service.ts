cd1f83de3ba0738f510b89640a76438f2ecd8178
AhmedDula <ahmedhacker301@gmail.com>
2026-08-05 18:59:11 +0300

    add disputes service

--- frontend/src/app/modules/disputes/dispute.service.ts
+++ frontend/src/app/modules/disputes/dispute.service.ts
@@ -1,11 +1,33 @@
-import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { Injectable, Service, inject } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { ApiService } from '../../core/services/api.service';
+
+import { DisputeResponse, IDispute } from './dispute.model';
+import { Observable } from 'rxjs';
+
+@Service()
 export class DisputeService {
-  private readonly http = inject(HttpClient);
-  private readonly baseUrl = `${environment.apiUrl}/disputes`;
+  private readonly api = inject(ApiService)
+
+
+  getDisputes(): Observable<DisputeResponse> {
+    return this.api.get<DisputeResponse>(`/disputes`);
+  }
+
+  getDisputeById(id: string): Observable<DisputeResponse> {
+    return this.api.get<DisputeResponse>(`/disputes/${id}`);
+  }
+
+  createDispute(dispute: IDispute): Observable<DisputeResponse> {
+    return this.api.post<DisputeResponse>(`/disputes`, dispute)
+
+  }
+
+  updateDispute(id: string, dispute: DisputeResponse): Observable<DisputeResponse> {
+    return this.api.put<DisputeResponse>(`/disputes/${id}`, dispute);
+  }
+
+  deleteDispute(id: string): Observable<DisputeResponse> {
+    return this.api.delete<DisputeResponse>(`/disputes/${id}`);
+  }
 }
